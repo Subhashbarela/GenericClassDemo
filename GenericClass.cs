7b288a8f1bd2c0452d07fa04a0b6897e50667443
Subhash Barela <barelasubhash16@gmail.com>
2023-05-09 09:05:37 +0530

    create generic class program

---
+++ GenericClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication
+{
+    public class GenericClass<T>
+    {
+        //this is variable of type T
+        public T data;
+
+        //This is my generic constructor
+        public GenericClass(T data)
+        {
+            this.data = data;
+            Console.WriteLine("Data : "+this.data);
+        }
+        public void GenaralcMethod(T data)
+        {
+            Console.WriteLine("Data : "+data);
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication
+{
+    public class Program
+    {
+        //Generic meanse the general form
+        //it meanse not specific for particuler data type
+        //<> this is angle becket with type name
+        //TypeName<T> T is here type parameter
+        //generic allowas to a create single class or method that can be use with different data type
+        //advantage :we can reuse our code
+        static void Main(string[] args)
+        {
+            GenericClass<String> stringObj = new GenericClass<string>("Subhash");
+            GenericClass<int> intObj = new GenericClass<int>(20);
+            GenericClass<double> doubleObj = new GenericClass<double>(20.10);
+
+            stringObj.GenaralcMethod("shiva");
+            intObj.GenaralcMethod(100);
+            doubleObj.GenaralcMethod(50.5);
+            Console.ReadLine();
+        }
+    }
+}
